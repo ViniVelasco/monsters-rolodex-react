07a3fe81e2344eb45fc0c6b859068eee34a72f15
= <sasukegc@gmail.com>
2019-07-30 13:48:24 -0300

    Updates

---
+++ precache-manifest.32716522c68a1a7e76c38b34d3238ada.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e499bb858ca2df01d8b3f3d8d08dc747",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "2d72a419da26dfba75ee",
+    "url": "/monsters-rolodex-react/static/css/main.6e769233.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2d72a419da26dfba75ee",
+    "url": "/monsters-rolodex-react/static/js/main.0bfab769.chunk.js"
+  },
+  {
+    "revision": "590cdc23d4720e222623",
+    "url": "/monsters-rolodex-react/static/js/runtime~main.56d562c4.js"
+  }
+]);
